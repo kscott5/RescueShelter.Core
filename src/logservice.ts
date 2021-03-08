3ccc822a2f6993c8dc8fa33883fca7feafea841a
Karega K. Scott <kkscott@outlook.com>
2021-03-07 19:09:08 -0600

    Add log file

--- src/logservice.ts
+++ src/logservice.ts
@@ -1,26 +1,37 @@
 import * as winston from "winston";
-import {DailyRotateFileTransportOptions} from "winston-daily-rotate-file";
+import DailyRotateFileTransportOptions from "winston-daily-rotate-file";
 
-const NODE_ENV = process.env?.NODE_ENV.toLowerCase() || "development";
-
-export function createLogService(label: string) {    
-    let logger = winston.createLogger();
+/**
+ * 
+ * @param {string} serviceName is a label identifier for a message
+ * @returns {winston.Logger}
+ */
+export function createLogService(serviceName: string) : winston.Logger {    
+    let logger = winston.createLogger({
+        format: winston.format.combine(
+            winston.format.label({label: serviceName, message: true}),
+            winston.format.prettyPrint(),
+            winston.format.timestamp(),            
+            winston.format.json()
+        ),
+        transports: [
+            new (winston.transports.DailyRotateFile)({
+                json: true,
+                dirname: process.env?.NODE_ENV_LOG_DIR || '.',
+                filename: 'combined-%DATE%.log',
+                datePattern: 'YYYY-MM-DD-HH',
+                zippedArchive: true,
+                maxSize: '20m',
+                maxFiles: '14d'
+              })
+        ]
+    });
 
     logger.clear();
     if(process.env?.NODE_ENV == 'development')
         logger.add(new winston.transports.Console);
     
-    logger.add(
-        new (winston.transports.DailyRotateFile)({
-            json: true,
-            dirname: process.env?.NODE_ENV_LOG_DIR || '.',
-            filename: 'combined-%DATE%.log',
-            datePattern: 'YYYY-MM-DD-HH',
-            zippedArchive: true,
-            maxSize: '20m',
-            maxFiles: '14d'
-          })
-    );
+    logger.add(new winston.transports.File);
 
     return logger;
 }
