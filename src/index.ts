5ab2f1947563a5c613ed1813f622dc1c61e84aab
Karega K. Scott <kkscott@outlook.com>
2025-04-02 12:48:36 -0500

    Expose create logger. Note: Server out process context use redis not (asyncLocalStorage in node:async_hook

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import server = require("./server");
 import services = require("./services");
+import { createLogService } from "./logservice";
 
 export {server as CoreServer};
 export {services as CoreServices};
+export {createLogService};
