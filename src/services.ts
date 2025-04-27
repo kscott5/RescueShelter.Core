525fa4f4540c8327b012ca671e1ce1654f09395f
Karega K. Scott <kkscott@outlook.com>
2025-04-26 21:38:34 -0500

    type definition and simple scaffolding models with the on 'connected' event emitters

--- src/services.ts
+++ src/services.ts
@@ -1,4 +1,4 @@
-import { Mongoose, Schema, Model, Document} from "mongoose";
+import { Mongoose, Connection} from "mongoose";
 
 const mongoose = new Mongoose()
 
@@ -10,70 +10,57 @@ if((process.env?.NODE_ENV || 'production') != 'production')
 const connectionUri = process.env?.RS_CONNECTION_URI || 'mongodb://localhost:27017/rescueshelter';
 const connection = mongoose.createConnection(connectionUri);
 
-const SECURITY_MODEL_NAME = "token";
-const securitySchema = createMongooseModel(SECURITY_MODEL_NAME, 
-    createMongooseSchema({}, false /* disable schema strict */));
+connection.once("connected", () => {
+    // create the model from the schema below
+    connection.model("animal", animalSchema);
+    connection.model("sponson", sponsorSchema);
+    connection.model("security", tokenSchema);
+    connection.model("audit", auditSchema);
 
-const TRACK_MODEL_NAME = "audit";
-const trackSchema = createMongooseModel(TRACK_MODEL_NAME, () => {
-    var schema = createMongooseSchema({
-            name: {type: String, required: true},
-            sponsor_id: {type: {}, required: true},
-            data: {type: {}, required: true},
-            date: {type: Date, required: true}
-        }); 
-
-    schema.path("data").default(new Date());
-
-    return schema;
+    connection.close();
 });
 
-const SPONSOR_MODEL_NAME = "sponsor";
-const sponsorSchema = createMongooseModel(SPONSOR_MODEL_NAME, ()=>{    
-    var question = createMongooseSchema({
-        _id: false,
-        question: {type: String, required: true},
-        answer: {type: String, required: true},
-    });
+const tokenSchema = new mongoose.Schema({}, {strict: false});
 
-    var securitySchema = createMongooseSchema({        
+const auditSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    sponsor_id: {type: {}, required: true},
+    data: {type: {}, required: true},
+    date: {type: Date, required: true}
+}, {strict: true}); 
+auditSchema.path("data").default(new Date());
+
+const sponsorSchema = new mongoose.Schema({        
+    firstname: {type: String},
+    lastname: {type: String},
+    useremail: {type: String, required: [true, '*'], unique: true},
+    username: {type: String, unique: true},
+    security: {
         _id: false,
         password: {type: String, required: true},
-        questions: [question]
-    });
-
-
-    var schema = createMongooseSchema({        
-        firstname: {type: String},
-        lastname: {type: String},
-        useremail: {type: String, required: [true, '*'], unique: true},
-        username: {type: String, unique: true},
-        security: {type: securitySchema},
-        photo: {type: String},
-        audit: [
-            {
-                _id: false,
-                modified: {type: Date, required: [true]},
-                sponsor_id: {type: mongoose.SchemaTypes.ObjectId, required: [true]}
-            }
-        ]
-    });
+        questions: [{
+            _id: false, question: {type: String, required: true},answer: {type: String, required: true}}]
+    },
+    photo: {type: String},
+    audit: [
+        {
+            _id: false,
+            modified: {type: Date, required: [true]},
+            sponsor_id: {type: mongoose.SchemaTypes.ObjectId, required: [true]}
+        }
+    ]
+}, {strict: true});
     
-    schema.index({username: "text", useremail: "text"});
-    schema.path("audit").default(function(){
-        return {
-            modified: Date.now(),
-            sponsor_id: this._id,
-        };
-    });    
-    //schema.path("audit.sponssor_id").default(function(){return Date.now();});
-    
-    return schema;
-});
+sponsorSchema.index({username: "text", useremail: "text"});
+sponsorSchema.path("audit").default(function(){
+    return {
+        modified: Date.now(),
+        sponsor_id: this._id,
+    };
+});    
+//sponsorSchema.path("audit.sponssor_id").default(function(){return Date.now();});
 
-const ANIMAL_MODEL_NAME = "animal";
-const animalSchema = createMongooseModel(ANIMAL_MODEL_NAME, ()=>{
-    var schema = createMongooseSchema({
+const animalSchema = new mongoose.Schema({
         name: {type: String, unique:true, required: [true, '*']},
         image: {
             content: String,
@@ -89,46 +76,11 @@ const animalSchema = createMongooseModel(ANIMAL_MODEL_NAME, ()=>{
             modified: Date
         },
         sponsors: {type: Array<String>()}
-    });
+    }, {strict: true});
     
-    schema.index({category_id: "asc", name: "text", category: "text", description: "text", sponsors: "text"});
-    schema.path("dates.created").default(function(){return Date.now();});
-    schema.path("dates.modified").default(function(){return Date.now();});
-    
-    return schema;
-});    
-
-/**
- * @description Create a new mongoose schema
- * 
- * @param schemaDefinition 
- * @param strictMode enables strict type reference
- * @returns new mongoose.Schema
- */
-export function createMongooseSchema(schemaDefinition: any, strictMode: boolean = true) {
-    return new mongoose.Schema(schemaDefinition, {strict: strictMode});
-}
-
-/**
- * @description Creates a new mongoose model from the schema
- * 
- * @param name used in creation of new mongoose model with schema
- * @param schema  a map or function that returns a map with schema defination
- * @returns new mongoose model document specific to the collection
- */
-export function createMongooseModel(name: ModelName, schema: Schema<any> | Function) 
-: Model<Document> {
-    return connection.model(name, (typeof schema == 'function')?  schema(): schema);
-}
-
-/**
- * @description retrieves the model name
- * @param name used in location of a readonly model
- * @returns existing mongoose model document
- */
-export function getModel(name: ModelName) : Model<Document> {    
-    return connection.model(name);
-}
+animalSchema.index({category_id: "asc", name: "text", category: "text", description: "text", sponsors: "text"});
+animalSchema.path("dates.created").default(function(){return Date.now();});
+animalSchema.path("dates.modified").default(function(){return Date.now();});
 
 export function createFindOneAndUpdateOptions(fields?: Object|String, upsert: boolean = false) {
     // MongoDB https://mongodb.github.io/node-mongodb-native/3.2/api/Collection.html#findOneAndUpdate
@@ -155,24 +107,15 @@ export function createFindOneAndUpdateOptions(fields?: Object|String, upsert: bo
 export type ModelName = "animal" | "audit" | "sponsor" |"token";
 
 /**
- * @description Predefine schema on token model
+ * @description create new database connection
  */
-module.exports = securitySchema;
+export function createConnection(connectionUrl?: string) : Connection {
+    const connectionUri = connectionUrl || process.env?.RS_CONNECTION_URI || 'mongodb://localhost:27017/rescueshelter';
+    const connection = mongoose.createConnection(connectionUri);
+    
+    return connection;
+}
 
-/**
- * @description Predefine audit schema on audit model
- */
-module.exports = trackSchema;
-
-/**
- * @description Prefined schema on sponsor model
- */
-module.exports = sponsorSchema;
-
-/**
- * @description Prefined schema on animal model
- */
-module.exports = animalSchema;
 /**
  * Pagaination class helper
  */
@@ -189,7 +132,7 @@ export class Pagination {
 } // end Pagination
 
 /**
- * Json server response helpter
+ * Json server response helper
  */
 export class JsonResponse {
     constructor(){}
